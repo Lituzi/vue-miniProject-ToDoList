20ad0eb09185f5bfebd205934b2970d970456828
Lituzi <li_tuzikou@163.com>
2021-04-01 22:11:16 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <to-do-list></to-do-list>
+  </div>
+</template>
+
+<script>
+import ToDoList from './components/toDoList'
+export default {
+  name: 'App',
+  components:{
+    ToDoList,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
